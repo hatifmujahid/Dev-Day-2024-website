6cf3f31ab9a34cbac170faaf3e9562b8e9509552
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-03-24 16:19:52 +0500

    home hero updated

---
+++ src/components/Home/FypExtreme.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image1 from "../../assets/temp1.png";
+
+
+const FypExtreme = () => {
+  return (
+    <div>
+      <div class="relative w-full h-screen overflow-hidden">
+        <img src={Image1} alt="" class="w-full h-full object-cover" />
+        <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60">
+          <h1 class="text-4xl sm:text-7xl lg:text-[120px] font-black text-white">
+            FYP Extreme
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FypExtreme;
