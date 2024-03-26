3117efe9fd62ff9ddf65bd1918e7409b02ea07ed
Sameer <111224603+SameerVers3@users.noreply.github.com>
2024-03-26 22:19:10 +0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
         { id: 1, text: 'Home', href: '/' },
         // { id: 2, text: 'About Us', href: '/about' },
         { id: 3, text: 'Register', href: '/register' },
-        {id: 4, text: 'ChatBot', href: '/chatBot'}
+        //{id: 4, text: 'ChatBot', href: '/chatBot'}
     ]
 
     return (
